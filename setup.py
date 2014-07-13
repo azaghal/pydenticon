eea16de02db545d9c1f8bdd28fbe3374fa8cc36e
Branko Majic <branko@majic.rs>
2014-07-13 22:48:28 +0200

    Noticket: Moving to development for version 0.2.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='pydenticon',
-    version='0.1.1',
+    version='0.2-dev',
     packages=['pydenticon'],
     include_package_data=True,
     license='BSD',  # example license
